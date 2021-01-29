6f185e1730cbadd4583d5c679adae13458c65d92
donneger <61294902+donneger@users.noreply.github.com>
2021-01-29 09:45:45 +0000

    Update bmi.py

--- Week2/bmi.py
+++ Week2/bmi.py
@@ -1,10 +1,10 @@
 # Program to calculate BMI for pands Lab 2.2
 # Author: Gerry Donnelly
-# Program takes Height and Weight inputs from the user and outputs the BMI score and indicates if Low Normal or High
+# Program takes Height and Weight inputs from the user and outputs the BMI score and indicates if Low, Normal or High
 
 
 H = int(input("Please enter your height in CM: "))
-W = int(input("Pleasde enter your weight in KG: "))
+W = int(input("Please enter your weight in KG: "))
 BMI = W/((H/100)*2) # Calculate the BMI
 if BMI<18.5: # Check if it is low
     print ("Your BMI is " + str(BMI) + " and is LOW ")
